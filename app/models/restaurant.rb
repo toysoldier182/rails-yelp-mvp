092cc7412b5da344ee62b29cb91cb40cd570f5ad
juancarlosasensio <jasensio@ufm.edu>
2020-02-19 13:28:02 +0000

    Create db:seeds file

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
-  cuisine = %w[chinese italian japanese french belgian]
+  CUISINE = %w[chinese italian japanese french belgian]
   has_many :reviews, dependent: :destroy
   validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: cuisine }
+  validates :category, inclusion: { in: CUISINE }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+50.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: Restaurant::CUISINE.sample
+  )
+  puts "Restaurant #{restaurant.name} has been added"
+end
+
+puts 'finished'
